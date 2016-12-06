8bb16e2222f707ad72bb8053a001e13fee3fe830
HoonyR <kwakjh1503@ajou.ac.kr>
2016-12-06 17:05:59 +0900

    report : ActivityAdder code
    Signed-off-by: HoonyR <kwakjh1503@ajou.ac.kr>

---
+++ ActivityAdder.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ActivityAdder : MonoBehaviour {
+
+    public DBManager dbmanager;
+    public Dropdown activity_type;
+    public Dropdown activity_start_h;
+    public Dropdown activity_start_m;
+    public Dropdown activity_end_h;
+    public Dropdown activity_end_m;
+    public void OnOK()
+    {
+        dbmanager.Insert("" + activity_type.value,
+            "" + activity_start_h.value,
+            "" + activity_start_m.value,
+            "" + activity_end_h.value,
+            ""+activity_end_m.value);
+    }
+}
