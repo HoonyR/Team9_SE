ae810a72b264a34f1e48a775f5fefa79db5be1f7
HoonyR <kwakjh1503@ajou.ac.kr>
2016-12-06 17:07:34 +0900

    report : ActivityManager code
    Signed-off-by: HoonyR <kwakjh1503@ajou.ac.kr>

---
+++ ActivityManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ActivityManager : MonoBehaviour {
+    public GameObject ActivityAddWindow;
+    public GameObject ActivityModifyWindow;
+    public GameObject ActivityDeleteWindow;
+    public GameObject ActivityViewWindow;
+    public DBManager dbmanager;   
+
+    public void ActivityAddOpen()
+    {
+        ActivityAddWindow.SetActive(true);
+   
+    }
+    public void ActivityAddClose()
+    {
+        ActivityAddWindow.SetActive(false);
+    }
+    public void ActivityModifyOpen()
+    {
+        ActivityModifyWindow.SetActive(true);
+    }
+    public void ActivityModifyClose()
+    {
+        ActivityModifyWindow.SetActive(false);
+    }
+    public void ActivityDeleteOpen()
+    {
+        ActivityDeleteWindow.SetActive(true);
+    }
+    public void ActivityDeleteClose()
+    {
+        ActivityDeleteWindow.SetActive(false);
+    }
+    public void ActivityViewOpen()
+    {
+        ActivityViewWindow.SetActive(true);
+        dbmanager.Select();
+    }
+    public void ActivityViewClose()
+    {
+        ActivityViewWindow.SetActive(false);
+    }
+}
