0255c77ef4513ca18bbbaed7874025b283698a9c
HoonyR <kwakjh1503@ajou.ac.kr>
2016-12-07 01:38:02 +0900

    report: Scene Loader code
    Signed-off-by: HoonyR <kwakjh1503@ajou.ac.kr>

---
+++ SceneLoader.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour {
+
+	public void OnStartClick()
+    {
+        SceneManager.LoadScene("Scenes/SelectModel");
+    }
+}
