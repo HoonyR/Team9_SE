a1132c2c4d4fe14ab9c8becbad1f69035bc3c231
HoonyR <kwakjh1503@ajou.ac.kr>
2016-12-06 17:11:48 +0900

    report: shopping manager code
    Signed-off-by: HoonyR <kwakjh1503@ajou.ac.kr>

---
+++ ShoppingManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoppingManager : MonoBehaviour {
+    public GameObject BuyWindow;
+    public GameObject AssetsWindow;
+   
+    public void BuyOpen()
+    {
+        BuyWindow.SetActive(true);
+    }
+    public void BuyClose()
+    {
+        BuyWindow.SetActive(false);
+    }
+    public void AssetsOpen()
+    {
+        AssetsWindow.SetActive(true);
+    }
+    public void AssetsClose()
+    {
+        AssetsWindow.SetActive(false);
+    }
+}
