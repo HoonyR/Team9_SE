1388ccd34c8b08d46c9f85e3eb4b91ca5421fb97
HoonyR <kwakjh1503@ajou.ac.kr>
2016-12-07 01:38:27 +0900

    report: updater code
    Signed-off-by: HoonyR <kwakjh1503@ajou.ac.kr>

---
+++ Updater.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Updater : MonoBehaviour {
+
+    public PetManager petmanager;
+    public ShoppingManager shopmanager;
+    void Start()
+    {
+        OnUpdate();
+    }
+    public void OnUpdate()
+    {
+        petmanager.UpdateState();
+        shopmanager.UpdateAssets();
+    }
+}
